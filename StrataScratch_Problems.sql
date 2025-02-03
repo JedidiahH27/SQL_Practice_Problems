b0ad788ca4b7bdfc74496b6bf37b44026be3868d
Jedidiah Hernandez <jediboaz27@gmail.com>
2025-02-02 23:31:24 -0800

    Update StrataScratch_Problems.sql

--- StrataScratch_Problems.sql
+++ StrataScratch_Problems.sql
@@ -6,7 +6,7 @@ SELECT EXTRACT(YEAR FROM inspection_date) AS inspection_year,
 FROM sf_restaurant_health_violations
 WHERE violation_id IS NOT NULL AND business_name = 'Roxanne Cafe'
 GROUP BY EXTRACT(YEAR FROM inspection_date)
-ORDER BY 1;
+ORDER BY inspection_year;
 
 ---------------------------------------------------------------------------------------------------------------------------
 
@@ -18,3 +18,29 @@ WHERE EXTRACT(MONTH FROM joining_date) >= 4 AND
       department = 'Admin';
 
 ---------------------------------------------------------------------------------------------------------------------------
+
+https://platform.stratascratch.com/coding/9847-find-the-number-of-workers-by-department/submissions?code_type=1
+       
+SELECT department, 
+       COUNT(*) AS n_workers
+FROM worker
+WHERE (EXTRACT(YEAR FROM joining_date) = 2014 AND
+      EXTRACT(MONTH FROM joining_date) >= 4) OR 
+      EXTRACT(YEAR FROM joining_date) >= 2015
+GROUP BY department
+ORDER BY n_workers DESC
+
+---------------------------------------------------------------------------------------------------------------------------
+
+https://platform.stratascratch.com/coding/9871-highly-reviewed-hotels?code_type=1
+       
+SELECT hotel_name, 
+       total_number_of_reviews 
+FROM hotel_reviews
+GROUP BY hotel_name, 
+         total_number_of_reviews
+ORDER BY total_number_of_reviews DESC;
+
+---------------------------------------------------------------------------------------------------------------------------
+
+
