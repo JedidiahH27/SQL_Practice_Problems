4c3a121b190066058b03a3f9e7288dd51caa2c64
Jedidiah Hernandez <jediboaz27@gmail.com>
2025-02-04 02:04:06 -0800

    Update StrataScratch_Problems.sql

--- StrataScratch_Problems.sql
+++ StrataScratch_Problems.sql
@@ -1,5 +1,5 @@
 ---------------------------------------------------------------------------------------------------------------------------
-https://platform.stratascratch.com/coding/9728-inspections-that-resulted-in-violations/submissions?code_type=1
+https://platform.stratascratch.com/coding/9728-inspections-that-resulted-in-violations?code_type=1
 
 SELECT EXTRACT(YEAR FROM inspection_date) AS inspection_year,
        COUNT(*) AS n_violations
@@ -19,7 +19,7 @@ WHERE EXTRACT(MONTH FROM joining_date) >= 4 AND
 
 ---------------------------------------------------------------------------------------------------------------------------
 
-https://platform.stratascratch.com/coding/9847-find-the-number-of-workers-by-department/submissions?code_type=1
+https://platform.stratascratch.com/coding/9847-find-the-number-of-workers-by-department?code_type=1
        
 SELECT department, 
        COUNT(*) AS n_workers
@@ -43,4 +43,28 @@ ORDER BY total_number_of_reviews DESC;
 
 ---------------------------------------------------------------------------------------------------------------------------
 
+https://platform.stratascratch.com/coding/9891-customer-details?code_type=1
 
+SELECT c.first_name, 
+       c.last_name,
+       c.city,
+       o.order_details
+FROM customers AS c
+LEFT JOIN orders AS o ON c.id = o.cust_id
+ORDER BY c.first_name, o.order_details;
+
+---------------------------------------------------------------------------------------------------------------------------
+
+https://platform.stratascratch.com/coding/9913-order-details?code_type=1
+
+SELECT c.first_name,
+       o.order_date,
+       o.order_details,
+       o.total_order_cost 
+FROM customers AS c
+INNER JOIN orders AS o ON c.id = o.cust_id
+WHERE c.first_name = 'Jill' OR 
+      c.first_name = 'Eva'
+ORDER BY c.id;
+
+---------------------------------------------------------------------------------------------------------------------------
