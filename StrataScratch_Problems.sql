7f1b4049262bbff2e263c29e17a0be2004f51662
Jedidiah Hernandez <jediboaz27@gmail.com>
2025-02-06 00:05:02 -0800

    Update StrataScratch_Problems.sql

--- StrataScratch_Problems.sql
+++ StrataScratch_Problems.sql
@@ -109,7 +109,8 @@ WHERE jobtitle = 'CAPTAIN III (POLICE DEPARTMENT)';
 
 https://platform.stratascratch.com/coding/9992-find-artists-that-have-been-on-spotify-the-most-number-of-times?code_type=1
 
-SELECT artist, COUNT(*) AS n_occurences
+SELECT artist, 
+       COUNT(*) AS n_occurences
 FROM spotify_worldwide_daily_song_ranking
 GROUP BY artist
 ORDER BY n_occurences DESC;
