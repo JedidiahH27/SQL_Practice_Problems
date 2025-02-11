1b0284cfb4947feb4deb31e19f28f047dc6629d2
Jedidiah Hernandez <jediboaz27@gmail.com>
2025-02-10 22:55:28 -0800

    Update StrataScratch_Problems.sql

--- StrataScratch_Problems.sql
+++ StrataScratch_Problems.sql
@@ -161,3 +161,25 @@ FROM oscar_nominees
 WHERE nominee = 'Abigail Breslin';
 
 ---------------------------------------------------------------------------------------------------------------------------
+
+https://platform.stratascratch.com/coding/10166-reviews-of-hotel-arena?code_type=1
+
+SELECT hotel_name,
+       reviewer_score,
+       COUNT(*)
+FROM hotel_reviews
+WHERE hotel_name = 'Hotel Arena'
+GROUP BY reviewer_score, hotel_name;
+
+---------------------------------------------------------------------------------------------------------------------------
+
+https://platform.stratascratch.com/coding/10176-bikes-last-used?code_type=1
+
+SELECT bike_number, 
+       MAX(end_time) AS last_used
+FROM dc_bikeshare_q1_2012
+GROUP BY bike_number
+ORDER BY MAX(end_time) DESC;
+
+---------------------------------------------------------------------------------------------------------------------------
+
