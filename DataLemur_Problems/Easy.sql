3c1fc851f26c5c6d47a61bcc8e68baf5392b06ce
Jedidiah Hernandez <jediboaz27@gmail.com>
2025-05-17 23:03:24 -0700

    Update Easy.sql

--- DataLemur_Problems/Easy.sql
+++ DataLemur_Problems/Easy.sql
@@ -1,1 +1,18 @@
+--------------------------------------------------------------------------------------------------------------------------------
+
+https://datalemur.com/questions/sql-histogram-tweets
+
+WITH number_of_tweets AS (
+  SELECT COUNT(*) AS tweet_bucket 
+  FROM tweets
+  WHERE EXTRACT(YEAR FROM tweet_date) = 2022
+  GROUP BY user_id
+  )
+
+SELECT tweet_bucket, 
+       COUNT(*) AS users_num
+FROM number_of_tweets
+GROUP BY tweet_bucket
+
+--------------------------------------------------------------------------------------------------------------------------------
 
