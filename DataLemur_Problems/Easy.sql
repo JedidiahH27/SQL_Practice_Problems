35fe2732c8da785484394849ac8255b700dacdf8
Jedidiah Hernandez <jediboaz27@gmail.com>
2025-05-20 23:38:09 -0700

    Update Easy.sql

--- DataLemur_Problems/Easy.sql
+++ DataLemur_Problems/Easy.sql
@@ -43,3 +43,13 @@ FROM parts_assembly
 WHERE finish_date IS NULL;
 
 --------------------------------------------------------------------------------------------------------------------------------
+
+https://datalemur.com/questions/laptop-mobile-viewership
+
+SELECT 
+SUM(CASE WHEN device_type = 'laptop' THEN 1 ELSE 0 END) AS laptop_views,
+SUM(CASE WHEN device_type = 'tablet' OR device_type = 'phone' THEN 1 ELSE 0 END) AS mobile_views
+FROM viewership
+
+--------------------------------------------------------------------------------------------------------------------------------
+
