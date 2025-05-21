24e76c790bf0eac1ca61edf2442b681d9aa89f66
Jedidiah Hernandez <jediboaz27@gmail.com>
2025-05-21 12:51:48 -0700

    Update Easy.sql

--- DataLemur_Problems/Easy.sql
+++ DataLemur_Problems/Easy.sql
@@ -62,3 +62,14 @@ GROUP BY user_id
 HAVING EXTRACT(DOY FROM MAX(post_date)) - EXTRACT(DOY FROM MIN(post_date)) > 0
 
 --------------------------------------------------------------------------------------------------------------------------------
+
+https://datalemur.com/questions/teams-power-users
+
+SELECT sender_id, COUNT(*) AS message_count
+FROM messages
+WHERE TO_CHAR(sent_date, 'YYYY-MM') = '2022-08'
+GROUP BY sender_id
+ORDER BY message_count DESC
+LIMIT 2
+
+--------------------------------------------------------------------------------------------------------------------------------
