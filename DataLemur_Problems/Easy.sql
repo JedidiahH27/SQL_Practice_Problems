51d824bd412c385476f322d895eab7373130437b
Jedidiah Hernandez <jediboaz27@gmail.com>
2025-05-20 23:42:57 -0700

    Update Easy.sql

--- DataLemur_Problems/Easy.sql
+++ DataLemur_Problems/Easy.sql
@@ -53,3 +53,12 @@ FROM viewership
 
 --------------------------------------------------------------------------------------------------------------------------------
 
+https://datalemur.com/questions/sql-average-post-hiatus-1
+
+SELECT user_id, EXTRACT(DOY FROM MAX(post_date)) - EXTRACT(DOY FROM MIN(post_date)) AS days_between
+FROM posts
+WHERE EXTRACT(YEAR FROM post_date) = 2021
+GROUP BY user_id
+HAVING EXTRACT(DOY FROM MAX(post_date)) - EXTRACT(DOY FROM MIN(post_date)) > 0
+
+--------------------------------------------------------------------------------------------------------------------------------
