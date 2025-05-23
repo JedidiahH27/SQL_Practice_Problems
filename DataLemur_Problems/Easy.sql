8b9293cb10dd1cea2443a7d3be5edf297d680120
Jedidiah Hernandez <jediboaz27@gmail.com>
2025-05-22 23:04:00 -0700

    Update Easy.sql

--- DataLemur_Problems/Easy.sql
+++ DataLemur_Problems/Easy.sql
@@ -102,3 +102,15 @@ LIMIT 3
 
 --------------------------------------------------------------------------------------------------------------------------------
 
+https://datalemur.com/questions/sql-avg-review-ratings
+
+SELECT EXTRACT(MONTH FROM submit_date) AS mth, 
+       product_id AS product, 
+       ROUND(AVG(stars), 2) AS avg_stars
+FROM reviews
+GROUP BY product_id, EXTRACT(MONTH FROM submit_date)
+ORDER BY mth, product
+
+--------------------------------------------------------------------------------------------------------------------------------
+
+
