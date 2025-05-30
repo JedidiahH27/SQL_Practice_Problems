4b8f5257088a23f60baa97f7b8b4a6c6ab8e6130
Jedidiah Hernandez <jediboaz27@gmail.com>
2025-05-29 22:49:06 -0700

    Update Medium.sql

--- DataLemur_Problems/Medium.sql
+++ DataLemur_Problems/Medium.sql
@@ -1,1 +1,27 @@
+------------------------------------------------------------------------------------------------
 
+https://datalemur.com/questions/sql-third-transaction
+
+WITH order_of_transactions AS (
+SELECT *, 
+       ROW_NUMBER() OVER(PARTITION BY user_id ORDER BY transaction_date) AS order_number
+FROM transactions
+)
+
+SELECT user_id, 
+       spend, 
+       transaction_date
+FROM order_of_transactions
+WHERE order_number = 3;
+
+------------------------------------------------------------------------------------------------
+
+https://datalemur.com/questions/sql-second-highest-salary
+
+SELECT DISTINCT salary AS second_highest_salary
+FROM employee
+ORDER BY salary DESC
+LIMIT 1
+OFFSET 1
+
+------------------------------------------------------------------------------------------------
